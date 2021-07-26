d83b6a4ffc9e69d102488bddfed414d49ab6bf3e
sixtown04 <66751765+sixtown04@users.noreply.github.com>
2021-07-25 19:11:30 -0500

    comentario

---
+++ src/java/Entidades/Comentario.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author sixto
+ */
+public class Comentario {
+
+    /**
+     * @return the Comentario
+     */
+    public String getComentario() {
+        return Comentario;
+    }
+
+    /**
+     * @param Comentario the Comentario to set
+     */
+    public void setComentario(String Comentario) {
+        this.Comentario = Comentario;
+    }
+
+    /**
+     * @return the Tema
+     */
+    public String getTema() {
+        return Tema;
+    }
+
+    /**
+     * @param Tema the Tema to set
+     */
+    public void setTema(String Tema) {
+        this.Tema = Tema;
+    }
+    
+   
+   
+    private String Comentario;
+    private String Tema;
+   
+  
+}
